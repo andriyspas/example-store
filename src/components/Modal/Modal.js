adeeca7df5adcfc7041e708d0a64270549f5b7fd
staspast <staspast1@gmail.com>
2017-10-21 13:15:47 +0200

    Modal state switch

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,107 +1,172 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import { Modal, Form, FormGroup, FormControl, Checkbox, Row, Col } from 'react-bootstrap';
+import { Modal, Form, FormGroup, FormControl, Checkbox, Row, Col } from 'react-bootstrap'
 
 class ModalStructure extends Component {
-    constructor() {
-        super();
+  constructor () {
+    super()
 
-        this.state = {
-            show: false
-        }
-    };
-
-    close = () => {
-        this.setState({show: false});
-    };
-
-    open = () => {
-        this.setState({show: true});
-    };
-
-    render() {
-        return (
-            <div>
-                <Modal
-                    show={ this.state.show }
-                    onHide={ this.close }
-                >
-
-                    <Modal.Body>
-
-                        <button onClick={ this.close }> X </button>
-
-                        <Form>
-                            <Row>
-                                <Col xs={12}>
-                                    <button className="button-modal button-modal__social button-modal__social--facebook">Facebook</button>
-                                    <button className="button-modal button-modal__social button-modal__social--google">Google</button>
-                                    <button className="button-modal button-modal__social button-modal__social--twitter">Twitter</button>
-                                </Col>
-                            </Row>
-
-                            <Row>
-                                <Col xs={12}>
-                                    <div className="divider"></div>
-                                </Col>
-                            </Row>
-
-                            <Row>
-                                <Col xs={12}>
-                                    <FormGroup>
-                                        <FormControl
-                                            className="input input__modal"
-                                            type="text"
-                                            placeholder="Name"
-                                        />
-                                    </FormGroup>
-
-                                    <FormGroup>
-                                        <FormControl
-                                            className="input input__modal"
-                                            type="email"
-                                            placeholder="Email"
-                                        />
-                                    </FormGroup>
-
-                                    <FormGroup>
-                                        <Checkbox>
-                                            Remember me
-                                        </Checkbox>
-                                    </FormGroup>
-
-                                    <button className="button-modal button-modal__login">Log in</button>
-
-                                    <button className="button-modal button-modal__forgot-password">Forward password</button>
-                                </Col>
-                            </Row>
-
-                            <Row>
-                                <Col xs={12}>
-                                    <div className="divider"></div>
-                                </Col>
-                            </Row>
-
-                            <Row>
-                                <Col xs={12}>
-                                    <span>Don’t have an account?</span>
-                                    <button className="button-modal button-modal__signup">Sign up</button>
-                                </Col>
-                            </Row>
-                        </Form>
-                        
-                    </Modal.Body>
-                </Modal>
-
-                <button
-                    className="button"
-                    onClick={ this.open }
-                >
-                    Open
-                </button>
-            </div>
-        );
+    this.state = {
+      show: false,
+      stateName: 'signIn'
     }
+  };
+
+  close = () => {
+    this.setState({show: false})
+  }
+
+  open = () => {
+    this.setState({show: true})
+  }
+
+  flipState(state) {
+    this.setState({stateName: state})
+  }
+
+  render () {
+    return (
+      <div>
+        <Modal
+          show={ this.state.show }
+          onHide={ this.close }
+        >
+          {this.state.stateName === 'signIn' &&
+          <Modal.Body>
+
+
+            <button onClick={ this.close }> X</button>
+
+            <Form>
+              <Row>
+                <Col xs={12}>
+                  <button className="button-modal button-modal__social button-modal__social--facebook">Facebook</button>
+                  <button className="button-modal button-modal__social button-modal__social--google">Google</button>
+                  <button className="button-modal button-modal__social button-modal__social--twitter">Twitter</button>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col xs={12}>
+                  <div className="divider"></div>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col xs={12}>
+                  <FormGroup>
+                    <FormControl
+                      className="input input__modal"
+                      type="text"
+                      placeholder="Name"
+                    />
+                  </FormGroup>
+
+                  <FormGroup>
+                    <FormControl
+                      className="input input__modal"
+                      type="email"
+                      placeholder="Email"
+                    />
+                  </FormGroup>
+
+                  <FormGroup>
+                    <Checkbox>
+                      Remember me
+                    </Checkbox>
+                  </FormGroup>
+
+                  <button className="button-modal button-modal__login">Log in</button>
+
+                  <button className="button-modal button-modal__forgot-password">Forward password</button>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col xs={12}>
+                  <div className="divider"></div>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col xs={12}>
+                  <span>Don’t have an account?</span>
+                  <button className="button-modal button-modal__signup" onClick={() => this.flipState('signUp')}>Sign up</button>
+                </Col>
+              </Row>
+            </Form>
+
+          </Modal.Body>
+          }
+
+          {this.state.stateName === 'signUp' &&
+          <Modal.Body>
+
+            <Form>
+              <Row>
+                <Col xs={12}>
+                  <button className="button-modal button-modal__social button-modal__social--facebook">Facebook</button>
+                  <button className="button-modal button-modal__social button-modal__social--google">Google</button>
+                  <button className="button-modal button-modal__social button-modal__social--twitter">Twitter</button>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col xs={12}>
+                  <div className="divider"></div>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col xs={12}>
+                  <FormGroup>
+                    <FormControl
+                      className="input input__modal"
+                      type="text"
+                      placeholder="Name"
+                    />
+                  </FormGroup>
+
+                  <FormGroup>
+                    <FormControl
+                      className="input input__modal"
+                      type="email"
+                      placeholder="Email"
+                    />
+                  </FormGroup>
+
+                  <button className="button-modal button-modal__login">Log in</button>
+
+                  <button className="button-modal button-modal__forgot-password">Forward password</button>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col xs={12}>
+                  <div className="divider"></div>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col xs={12}>
+                  <span>Don’t have an account?</span>
+                  <button className="button-modal button-modal__signup" onClick={() => this.flipState('signIn')}>Sign in</button>
+                </Col>
+              </Row>
+            </Form>
+
+          </Modal.Body>}
+        </Modal>
+
+        <button
+          className="button"
+          onClick={ this.open }
+        >
+          Open
+        </button>
+      </div>
+    )
+  }
 }
 
-export default ModalStructure;
+export default ModalStructure
